48d207a9b3110003fe4670b03e36dd74848798c6
brand421 <57531511+brand421@users.noreply.github.com>
2023-05-22 13:13:28 -0400

    complete Spotify login authorization integration

--- app.js
+++ app.js
@@ -1,12 +1,44 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
-app.use(bodyParser.urlencoded({extended: true}));
+const querystring = require("querystring");
 
 const app = express();
-
+app.use(bodyParser.urlencoded({extended: true}));
 const url = "https://api.planningcenteronline.com/api/v2/personal_access_tokens";
 
+const api = require(__dirname + "/apiTokens.js");
+const redirectUri = "http://localhost:3000";
+
+var stateKey = 'spotify_auth_state';
+
+var generateRandomString = function(length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
 app.get("/", function(req, res) {
-    
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.get("/Spotifylogin", function(req, res) {
+    var state = generateRandomString(16);
+    var scope = 'user-read-private user-read-email';
+
+    res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: api.spotifyId,
+      scope: scope,
+      redirect_uri: redirectUri,
+      state: state
+    }));
+})
+
+app.listen(3000, function() {
+    console.log("Server is listening on port 3000");
 })
